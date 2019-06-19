eb0fda38a32fea0ad1bf7cb417e9b605785d8781
orvalho <rasa396@gmail.com>
2019-06-20 01:20:34 +0300

    Updates

---
+++ precache-manifest.abf81b31bf5d5b1bc8cc5318b1d09296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e976d11a2395b3b86fb5f81318188d",
+    "url": "/cat-facts-app/index.html"
+  },
+  {
+    "revision": "911e3c29902fa1301767",
+    "url": "/cat-facts-app/static/css/main.79360641.chunk.css"
+  },
+  {
+    "revision": "8a43a3611aed6c7f8bf4",
+    "url": "/cat-facts-app/static/js/2.7bbd3bb4.chunk.js"
+  },
+  {
+    "revision": "911e3c29902fa1301767",
+    "url": "/cat-facts-app/static/js/main.bde71ecb.chunk.js"
+  },
+  {
+    "revision": "193e02ba1b99201c4c2a",
+    "url": "/cat-facts-app/static/js/runtime~main.a3707873.js"
+  }
+]);
